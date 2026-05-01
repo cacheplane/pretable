41bb82b4cc9db9fb7e558706ae5f6e6e1ac38326
Brian Love <brian@liveloveapp.com>
2026-05-01 09:13:32 -0700

    feat(website): docs sidebar shell + nav config

---
+++ apps/website/app/docs/_nav.ts
@@ -0,0 +1,16 @@
+export interface DocsNavItem {
+  title: string;
+  href: string;
+}
+
+export interface DocsNavSection {
+  title: string;
+  items: DocsNavItem[];
+}
+
+export const docsNav: DocsNavSection[] = [
+  {
+    title: "Getting Started",
+    items: [{ title: "Install + first grid", href: "/docs/getting-started" }],
+  },
+];
