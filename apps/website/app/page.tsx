d5639d7fa3ffa6d44a467cdcaa370ca4b245913e
Brian Love <brian@liveloveapp.com>
2026-04-30 17:56:45 -0700

    feat(website): compose seven body sections in homepage

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,11 +1,25 @@
+import { CodeExample } from "./components/CodeExample";
+import { ComparisonTable } from "./components/ComparisonTable";
+import { CtaSection } from "./components/CtaSection";
+import { FeatureGrid } from "./components/FeatureGrid";
 import { Hero } from "./components/Hero";
 import { PlaygroundSection } from "./components/PlaygroundSection";
+import { Problem } from "./components/Problem";
+import { ReceiptsBand } from "./components/ReceiptsBand";
+import { Solution } from "./components/Solution";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <PlaygroundSection />
+      <Problem />
+      <Solution />
+      <ReceiptsBand />
+      <ComparisonTable />
+      <FeatureGrid />
+      <CodeExample />
+      <CtaSection />
     </>
   );
 }
